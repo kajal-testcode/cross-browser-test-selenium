38d7a61dcfaee8ecc688fac29c13c24f2c02bf98
kajal-testcode <31248808+kajal-testcode@users.noreply.github.com>
2017-08-22 13:03:49 -0400

    Add files via upload

---
+++ CrossBrowserTest.java
@@ -0,0 +1,67 @@
+package com.sample.code;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class CrossBrowserTest {
+	WebDriver driver;
+
+	/**
+	 * This function will execute before each Test tag in testng.xml
+	 * @param browser
+	 * @throws Exception
+	 */
+	@BeforeTest
+	@Parameters("browser")
+	public void setup(String browsername) throws Exception{
+		//Check if parameter passed from TestNG is 'firefox'
+		if(browsername.equalsIgnoreCase("firefox")){
+		//create firefox instance
+			System.setProperty("webdriver.gecko.driver", ".\\drivers\\geckodriver.exe");
+			driver = new FirefoxDriver();
+		}
+		//Check if parameter passed as 'chrome'
+		else if(browsername.equalsIgnoreCase("chrome")){
+			//set path to chromedriver.exe
+			System.setProperty("webdriver.chrome.driver",".\\drivers\\chromedriver.exe");
+			//create chrome instance
+			driver = new ChromeDriver();
+		}
+		//Check if parameter passed as 'Edge'
+				else if(browsername.equalsIgnoreCase("Edge")){
+					//set path to Edge.exe
+					System.setProperty("webdriver.edge.driver",".\\drivers\\MicrosoftWebDriver.exe");
+					//create Edge instance
+					driver = new EdgeDriver();
+				}
+		else{
+			//If no browser passed throw exception
+			throw new Exception("Browser is not correct");
+		}
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+	
+	@Test(alwaysRun=true)
+	public void testParameterWithXML() throws InterruptedException{
+		driver.get("http://demo.guru99.com/V4/");
+		//Find user name
+		WebElement userName = driver.findElement(By.name("uid"));
+		//Fill user name
+		userName.sendKeys("guru99");
+		//Find password
+		WebElement password = driver.findElement(By.name("password"));
+		//Fill password
+		password.sendKeys("guru99");
+	}
+	
+	
+}
